e772b5f1435a8c66c551e94d6ef18a56f4be6da0
Mayank_Kathiriya <125014964+MayankKathiriya@users.noreply.github.com>
2023-05-08 15:18:01 +0530

    Update Passport.local.strategy.js
    .

--- config/Passport.local.strategy.js
+++ config/Passport.local.strategy.js
@@ -1,33 +1,24 @@
 const passport = require("passport");
-
 const passportLocal = require("passport-local").Strategy;
-
 const RegisterModel = require("../models/Register.Model");
-
 passport.use(new passportLocal({
     usernameField: "email"
 }, async (email, password, done) => {
     try {
         const add = await RegisterModel.findOne({ email });
-            
         if (!add || add.password != password) {
-
             return done(null, false);
         }
         return done(null, add);
     } catch (error) {
         console.log(error.message);
     }
-
 }));
-
 passport.serializeUser((add, done) => {
     return done(null, add.id);
 });
-
 passport.deserializeUser(async (id, done) => {
     try {
-
         const data = await RegisterModel.findById(id);
         if (!data) {
             return done(null, false)
@@ -37,21 +28,16 @@ passport.deserializeUser(async (id, done) => {
         console.log(error.message);
     }
 });
-
 passport.checkAuthentication = (req, res, next) => {
     if (req.isAuthenticated()) {
         return next();
     }
     return res.redirect("/");
 }
-
 passport.setAuthentication = (req, res, next) => {
     if (req.isAuthenticated()) {
         res.locals.saveLoginData = req.user;
     }
     return next();
 }
-
-
-
 module.exports = passport;
