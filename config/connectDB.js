ad0077f32559d2e5b668238da23b45413169affa
Mayank_Kathiriya <125014964+MayankKathiriya@users.noreply.github.com>
2023-05-08 15:16:47 +0530

    Update connectDB.js

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
-
 const connectdb = async () => {
   const db = await mongoose.connect("mongodb+srv://mayankkathiriya008:36MgeSx1QkbV3e2z@nodejs.exnjezb.mongodb.net/FinalProject?retryWrites=true&w=majority");
-
   if(db){
     console.log("db connected");
   }else{
     console.log("db not connected");
   }
-
 }
-
 module.exports = connectdb;
